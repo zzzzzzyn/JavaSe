34d361c38d6997761a4658a527e7897cfca8dc53
Xuyn <xuyanan@diandongmark.com>
2020-01-06 17:33:25 +0800

    Singleton类的添加

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ JavaSe的笔记
 JavaSe
 ├── collection -- 常用集合
 ├── concurrent -- 并发包
+    ├── atomic -- 原子类
+    ├── blockingqueue -- 阻塞队列
+    ├── lock -- 锁
+    ├── singleton -- 几种单例实现方式
 ├── io -- io
 ├── java8 -- java8的一些新特性
 ├── map -- map的介绍
--- src/concurrent/singleton/Singleton1.java
+++ src/concurrent/singleton/Singleton1.java
@@ -1,4 +1,4 @@
-package concurrent.singleton;
+package concurrent.Singleton;
 
 /**
  * 单例
@@ -18,21 +18,16 @@ public class Singleton1 {
     }
 
     public static Singleton1 getInstance() {
-        if (uniqueSingleton == null) {                  // 1
+        // 1
+        if (uniqueSingleton == null) {
             synchronized (Singleton1.class) {
-                /**
-                 * 这里再次判断是考虑指令重排及执行三个过程间
-                 * 线程1的uniqueSingleton值分配了内存空间和初始化了对象还没有赋值
-                 * 此时线程2执行到语句1发现uniqueSingleton字段为null，开始尝试获取锁
-                 * 获取到锁后，uniqueSingleton字段也赋值好了
-                 * 所以获取锁后要重新判断是否赋值
-                 */
                 if (uniqueSingleton == null) {
                     /**
-                     * 执行的操作
-                     * 1. 分配内存空间
+                     * 1. 创建对象Singleton1
                      * 2. 初始化对象Singleton1
                      * 3. 为uniqueSingleton赋值
+                     * 考虑指令重排及执行三个过程间，可能在1出有线程进行判断，
+                     * 所以获取锁后要重新判断是否赋值
                      */
                     uniqueSingleton = new Singleton1();
                 }
